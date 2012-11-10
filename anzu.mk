b47ffe777d62d622a97a6ffac2c24b0f3fc03c76
n1kolaa <n1kolaa@hotmail.com>
2012-11-10 18:55:26 +0100

    Update anzu.mk

--- anzu.mk
+++ anzu.mk
@@ -15,7 +15,7 @@ PRODUCT_COPY_FILES += \
     device/semc/anzu/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/anzu/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/anzu/prebuilt/hw_config.sh:system/etc/hw_config.sh \
-    device/semc/msm7x30-common/prebuilt/sony-pac_480x854.rle:root/logo.rle
+    device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
     device/semc/anzu/prebuilt/bootrec-device:root/sbin/bootrec-device \
     device/semc/anzu/recovery.fstab:root/recovery.fstab
 
