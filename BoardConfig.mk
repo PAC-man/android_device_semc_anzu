f6f0685b7add25810acf4367c7a6085780972df1
n1kolaa <n1kolaa@hotmail.com>
2012-11-10 18:47:15 +0100

    Update BoardConfig.mk
    Initial pac code for anzu

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@
 -include vendor/semc/anzu/BoardConfigVendor.mk
 
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
-TARGET_KERNEL_CONFIG := cyanogen_anzu_defconfig
+TARGET_KERNEL_CONFIG := pac_anzu_defconfig
 #TARGET_PREBUILT_KERNEL := device/semc/anzu/prebuilt/kernel
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
